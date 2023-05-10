3d68f24c9d235b5088515c53feb02e9e7472be2a
liqing <liqing.leesin@bytedance.com>
2023-05-10 16:50:03 +0800

    feat: vite execise

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-let test: string = 'aaaa'
-console.log(test)
+import styles from './index.module.scss';
+import tickSvg from './asserts/tick.svg?raw';
+import { a } from './asserts/test.json';
+const root = document.getElementById('root');
+root?.classList.add(styles.root)
+root && (root.innerHTML = tickSvg);
+console.log(a, 'a')
