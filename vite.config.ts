f221bed79451b6a9c83349289c641aa6b2200ae0
liqing <1462270974@qq.com>
2023-05-25 16:41:27 +0800

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import postcssPresetEnv from 'postcss-preset-env';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -10,6 +11,12 @@ export default defineConfig(({ mode }) => {
         plugins: [postcssPresetEnv()]
       }
     },
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname)
+      },
+      extensions: ['.tsx']
+    }
   }
 });
